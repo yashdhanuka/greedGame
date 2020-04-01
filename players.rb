27ad4d94140b187e352712873bd079b64e167f48
yashdhanuka <59160937+yashdhanuka@users.noreply.github.com>
2020-04-01 19:45:53 +0530

    Update players.rb

--- players.rb
+++ players.rb
@@ -1,4 +1,5 @@
 require ('./score.rb')
+require 'pry'
 
 class Players < Score
     @@tc = 0
@@ -6,6 +7,7 @@ class Players < Score
     def initialize(players=1)
         @players = players
         @tc = Array.new(players,0)
+        @counter = Array.new(players,0)
     end
     def play(i,no_of_dice=5)
         @input = "n"
@@ -14,9 +16,10 @@ class Players < Score
       sc = score(@@num)
       puts "Score in this round: #{sc}"
       return "no more chance" if sc == 0
-      
+      @@tc = get_in_the_game(i-1,sc)
+      puts "total score of player#{i}: #{@@tc}"
       rem = non_scored_dice(@@num)
-      puts rem
+      puts "remaining dice: #{rem}"
       if rem!=0
         puts "Do you want to roll the non-scoring #{rem} dices?(y/n): "
         @input = gets.chomp
@@ -24,20 +27,31 @@ class Players < Score
       if @input =="y"
         play(i,rem)
       end
-      @@tc = total_score(i-1,sc)
-      puts "total score: #{@@tc}"
-    end
-    def total_score(i,num)
-        @tc[i] += num
     end
     def winner
         puts " score: #{@tc}"
         index = @tc.rindex(@tc.max)
         puts "The Winner is: player #{index+1}"
     end
+    def get_maxScore
+        return @tc.rindex(@tc.max)+1
+    end
     def getTotal
         @tc
     end
+    def get_in_the_game(i,score)
+        #binding.pry
+        #puts "counter: #{@counter}"
+        if score > 0 && @counter[i] >0
+            @tc[i] += score
+        end
+        if @counter[i] == 0 && score >=300
+            @counter[i] += 1
+            @tc[i] += score
+        end
+    end
+
+
 end
 
 
