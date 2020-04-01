ef797464dba231971e9830e181fc3fd7f4c953be
yashdhanuka <59160937+yashdhanuka@users.noreply.github.com>
2020-04-01 19:46:22 +0530

    Update greedGame.rb

--- greedGame.rb
+++ greedGame.rb
@@ -7,7 +7,7 @@ players = Players.new(no_of_players)
 max_score = 0
 round = 1
 
-while(max_score<3000)
+while(max_score<1000)
     puts "Round #{round}"
     puts "-----------------------------------------------------"
     for i in 1..no_of_players
@@ -15,11 +15,16 @@ while(max_score<3000)
     end
     max_score = players.getTotal.max
     round+=1
+    puts "total score of all players: #{players.getTotal}"
 end
-
+top_player = players.get_maxScore
+puts "top player: #{top_player}"
 puts "Final Round"
     puts "-----------------------------------------------------"
     for i in 1..no_of_players
+       if i == top_player
+        next
+       end
      players.play(i)
     end
 
